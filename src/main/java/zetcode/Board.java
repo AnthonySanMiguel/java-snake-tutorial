b0e05b4ebdc604ee2bf43d4c680728bdea49f18c
Anthony San Miguel <asanmiguel1088@gmail.com>
2020-08-29 13:43:24 -0500

    added comments for java snake tutorial code

--- src/main/java/zetcode/Board.java
+++ src/main/java/zetcode/Board.java
@@ -20,13 +20,26 @@ import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
 
+    // Defining the constants (variables that won't change)
+
+    // Determine the size of the 'board'/game window
     private final int B_WIDTH = 300;
     private final int B_HEIGHT = 300;
+
+    // The size of the 'apple' image as well as the size of the snake's body dots ('dot.png')
     private final int DOT_SIZE = 10;
+
+    // Defines the maximum number of possible dots on the board (900 = (300 * 300) / (10 * 10))
+                                                           // or 900 = (B_WIDTH * B_HEIGHT) / DOT SIZE * DOT SIZE)
     private final int ALL_DOTS = 900;
+
+    // Calculates a random position for each apple spawn
     private final int RAND_POS = 29;
+
+    // Determine the 'speed' of the game
     private final int DELAY = 140;
 
+    // These two arrays store the x and y coordinates of all joints of a snake.
     private final int x[] = new int[ALL_DOTS];
     private final int y[] = new int[ALL_DOTS];
 
@@ -46,23 +59,20 @@ public class Board extends JPanel implements ActionListener {
     private Image head;
 
     public Board() {
-
         initBoard();
     }
 
     private void initBoard() {
-
         addKeyListener(new TAdapter());
         setBackground(Color.black);
         setFocusable(true);
-
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
         loadImages();
         initGame();
     }
 
+    // The 'ImageIcon' class is used for displaying PNG images
     private void loadImages() {
-
         ImageIcon iid = new ImageIcon("src/resources/dot.png");
         ball = iid.getImage();
 
@@ -73,15 +83,14 @@ public class Board extends JPanel implements ActionListener {
         head = iih.getImage();
     }
 
+    // Create the snake, randomly spawn an apple on the board, and start the timer
     private void initGame() {
+        dots = 3; // Begin game with snake having 3 total 'dots' (2 body 'dots' plus the head 'dot')
 
-        dots = 3;
-
-        for (int z = 0; z < dots; z++) {
+        for (int z = 0; z < dots; z++) { // Randomly spawn an apple on the board
             x[z] = 50 - z * 10;
             y[z] = 50;
         }
-
         locateApple();
 
         timer = new Timer(DELAY, this);
@@ -91,16 +100,12 @@ public class Board extends JPanel implements ActionListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
         doDrawing(g);
     }
 
     private void doDrawing(Graphics g) {
-
         if (inGame) {
-
             g.drawImage(apple, apple_x, apple_y, this);
-
             for (int z = 0; z < dots; z++) {
                 if (z == 0) {
                     g.drawImage(head, x[z], y[z], this);
@@ -108,92 +113,90 @@ public class Board extends JPanel implements ActionListener {
                     g.drawImage(ball, x[z], y[z], this);
                 }
             }
-
             Toolkit.getDefaultToolkit().sync();
-
         } else {
-
             gameOver(g);
         }
     }
 
+    // Details for 'Game Over' screen window
     private void gameOver(Graphics g) {
-
         String msg = "Game Over";
         Font small = new Font("Helvetica", Font.BOLD, 14);
         FontMetrics metr = getFontMetrics(small);
-
         g.setColor(Color.white);
         g.setFont(small);
         g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2);
     }
 
     private void checkApple() {
-
+        // If the apple collides with the head...
         if ((x[0] == apple_x) && (y[0] == apple_y)) {
-
+            // Increase the number of joints ('dots') of the snake.
             dots++;
+            // Then call the locateApple() method which randomly positions a new apple object on the board.
             locateApple();
         }
     }
 
+ // The key algorithm of the game:
+     // We control the head of the snake.
+     // We change its direction with the cursor keys.
+     // The rest of the joints move one position up the chain.
+     // The second joint moves where the first was,
+     // the third joint where the second was, etc.
     private void move() {
-
+        // Moves the joints up the chain.
         for (int z = dots; z > 0; z--) {
             x[z] = x[(z - 1)];
             y[z] = y[(z - 1)];
         }
-
+        // Moves the head to the left.
         if (leftDirection) {
             x[0] -= DOT_SIZE;
         }
-
+        // Moves the head to the right.
         if (rightDirection) {
             x[0] += DOT_SIZE;
         }
-
+        // Moves the head up.
         if (upDirection) {
             y[0] -= DOT_SIZE;
         }
-
+        // Moves the head down.
         if (downDirection) {
             y[0] += DOT_SIZE;
         }
     }
 
+    // Determine if the snake has hit itself or one of the walls.
     private void checkCollision() {
-
         for (int z = dots; z > 0; z--) {
-
+            // If the snake hits one of its joints with its head, the game is over.
             if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
                 inGame = false;
                 break;
             }
         }
-
+        // Also, it is game over if the snake hits the bottom of the board.
         if (y[0] >= B_HEIGHT) {
             inGame = false;
         }
-
         if (y[0] < 0) {
             inGame = false;
         }
-
         if (x[0] >= B_WIDTH) {
             inGame = false;
         }
-
         if (x[0] < 0) {
             inGame = false;
         }
-
         if (!inGame) {
             timer.stop();
         }
     }
 
     private void locateApple() {
-
         int r = (int) (Math.random() * RAND_POS);
         apple_x = ((r * DOT_SIZE));
 
@@ -203,22 +206,17 @@ public class Board extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if (inGame) {
-
             checkApple();
             checkCollision();
             move();
         }
-
         repaint();
     }
 
     private class TAdapter extends KeyAdapter {
-
         @Override
         public void keyPressed(KeyEvent e) {
-
             int key = e.getKeyCode();
 
             if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
@@ -226,19 +224,16 @@ public class Board extends JPanel implements ActionListener {
                 upDirection = false;
                 downDirection = false;
             }
-
             if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
                 rightDirection = true;
                 upDirection = false;
                 downDirection = false;
             }
-
             if ((key == KeyEvent.VK_UP) && (!downDirection)) {
                 upDirection = true;
                 rightDirection = false;
                 leftDirection = false;
             }
-
             if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
                 downDirection = true;
                 rightDirection = false;
--- src/main/java/zetcode/Snake.java
+++ src/main/java/zetcode/Snake.java
@@ -3,28 +3,26 @@ package zetcode;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
+// 'Snake' is the main Java class
 public class Snake extends JFrame {
-
     public Snake() {
-
         initUI();
     }
 
     private void initUI() {
-
         add(new Board());
 
+        // The setResizable() method affects the insets of the JFrame container on some platforms.
+        // Therefore, it is important to call it BEFORE the pack() method, otherwise, the collision of the snake's head with the right and bottom borders might not work correctly.
         setResizable(false);
         pack();
 
-        setTitle("Snake");
+        setTitle("Snake"); // Game title to show on JFrame window
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-
     public static void main(String[] args) {
-
         EventQueue.invokeLater(() -> {
             JFrame ex = new Snake();
             ex.setVisible(true);
